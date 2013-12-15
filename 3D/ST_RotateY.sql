a3f108372787d9c9690a7bf9bf38b7a725fcf0fa
Stephen Mather <stephen@smathermather.com>
2013-12-14 20:40:39 -0500

    geometry ST_RotateY(geometry geomA, float rotRadians, geometry pointOrigin)

---
+++ 3D/ST_RotateY.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION ST_RotateY(geomA geometry, rotRadians double precision, pointOrigin geometry)
+  RETURNS geometry AS
+$BODY$
+
+----- Transform geometry to nullsville (0,0,0) so rotRadians will take place around the pointOrigin
+WITH transformed AS (
+    SELECT ST_Translate(geomA, -1 * ST_X(pointOrigin), -1 * ST_Y(pointOrigin), -1 * ST_Z(pointOrigin)) AS the_geom
+    ),
+----- Rotate in place
+rotated AS (
+    SELECT ST_RotateY(the_geom, rotRadians) FROM transformed
+    ),
+----- Translate back home
+rotTrans AS (
+    SELECT ST_Translate(geomA, ST_X(pointOrigin), ST_Y(pointOrigin), ST_Z(pointOrigin)) AS the_geom
+    )
+----- profit
+SELECT the_geom from rotTrans
+
+;
+
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
