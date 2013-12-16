cb41c8ae83ccc3309fdc1a4dfb98b7f0db49ec25
Stephen Mather <stephen@smathermather.com>
2013-12-15 23:43:13 -0500

    Create ST_RotateXYZ.sql
    Allows for rotation on 3-axis simultaneously, depends on alternate versions of ST_RotateX and ST_RotateY that can take pointOrigin

---
+++ 3D/ST_RotateXYZ.sql
@@ -0,0 +1,26 @@
+-- Function: st_rotatezyz(geometry, double precision, double precision, double precision, geometry)
+CREATE OR REPLACE FUNCTION ST_RotateX(geomA geometry, rotRadiansX double precision, rotRadiansY double precision, rotRadiansZ double precision, pointOrigin geometry)
+  RETURNS geometry AS
+$BODY$
+
+-- Rotate around X-axis
+WITH rotatedX AS (
+    SELECT ST_RotateX(geomA, rotRadiansX, pointOrigin) AS geom
+    ),
+-- Rotate around Y-axis
+rotatedXY AS (
+    SELECT ST_RotateY(geom, rotRadiansY, pointOrigin) AS geom
+      FROM rotatedX
+    ),
+-- Rotate around Z-axis
+rotatedXYZ AS (
+    SELECT ST_Rotate(geom, rotRadiansZ, pointOrigin) AS geom
+      FROM rotatedXY
+    )
+-- Return rotated geometry
+SELECT geom from rotatedXYZ
+;
+
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
